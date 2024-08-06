f5f7b45b919597852392eb1d550777eb0457bdd0
NunTheeKnee <jane6677doe@gmail.com>
2024-08-06 13:57:34 +0530

    Added the table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/about'
   root 'home#index'
+  get 'home/add'
   resources :patients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
