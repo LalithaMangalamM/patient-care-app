9a8b4d94076d8367913a7cabd15a3fed4f3189f0
NunTheeKnee <jane6677doe@gmail.com>
2024-08-12 11:48:49 +0530

    Updated the table to track the user and the timestamp

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,5 @@
 class PatientsController < ApplicationController
   before_action :authenticate_user!
-
   before_action :set_patient, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -20,6 +19,8 @@ class PatientsController < ApplicationController
 
   def create
     @patient = Patient.new(patient_params)
+    @patient.user_email = current_user.email
+    @patient.user_id = current_user.id
 
     if @patient.save
       redirect_to @patient, notice: 'Patient was successfully created.'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_09_101025) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_09_121623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_09_101025) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_email"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_09_101025) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "patients", "users"
 end
