7ae62ea1ea27f4917bcb5641ae4adee553d5e981
NunTheeKnee <jane6677doe@gmail.com>
2024-08-20 17:05:40 +0530

    Added the update functionality

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,33 +1,52 @@
 class DoctorsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+
   def index
     @doctors = Doctor.all
   end
+
   def new
-    @check = "This is a variable"
+    @doctor = Doctor.new
   end
+
   def create
     @doctor = Doctor.new(doctor_params)
     if @doctor.save
       redirect_to root_path, notice: "Doctor was saved successfully"
     else
-      redirect_to :new
+      render :new
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+      redirect_to doctor_path(@doctor), notice: "Doctor was updated successfully"
+    else
+      render :edit
+    end
+  end
+
   def show
-    @doctor = Doctor.find(params[:id])
   end
 
   def destroy
-    @doctor = Doctor.find(params[:id])
-    if @doctor.delete
-      redirect_to @doctor, notice: "Deleted sucessfully"
+    if @doctor.destroy
+      redirect_to doctors_path, notice: "Deleted successfully"
     else
-      redirect_to root_path, notice: "There was an error deleting file"
+      redirect_to root_path, alert: "There was an error deleting the doctor"
     end
   end
 
   private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
   def doctor_params
     params.require(:doctor).permit(:first_name, :last_name, :contact, :email, :degree)
   end
