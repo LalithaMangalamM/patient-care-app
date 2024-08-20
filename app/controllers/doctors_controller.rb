2360fa4fe7732fffca5cee3e2f2aa49cd9c42c9f
NunTheeKnee <jane6677doe@gmail.com>
2024-08-20 14:23:54 +0530

    Added the Doctors controller

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,7 @@
+class DoctorsController < ApplicationController
+  def index
+  end
+  def new
+    @check = "This is a variable"
+  end
+end
--- db/migrate/20240820084456_create_doctors.rb
+++ db/migrate/20240820084456_create_doctors.rb
@@ -1,4 +1,4 @@
-class CreateDoctor < ActiveRecord::Migration[7.1]
+class CreateDoctors < ActiveRecord::Migration[7.1]
   def change
     create_table :doctors do |t|
       t.string :first_name
