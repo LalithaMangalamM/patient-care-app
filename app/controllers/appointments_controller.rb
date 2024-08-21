2dfe3bafb61582cd2f3aa0bb0c247edc6af970d2
NunTheeKnee <jane6677doe@gmail.com>
2024-08-21 15:56:01 +0530

    CRUD operations can be performed on APpointments now

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -21,6 +21,21 @@ class AppointmentsController < ApplicationController
     end
   end
 
+  def show
+    @doctor = Doctor.find(Appointment.find(params[:id]).doctor_id)
+    @patient = Patient.find(Appointment.find(params[:id]).patient_id)
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    if @appointment.destroy
+      redirect_to appointments_path, notice: "Appointment deleted successfully"
+    else
+      render :show
+    end
+  end
+
   private
 
   def appointment_params
