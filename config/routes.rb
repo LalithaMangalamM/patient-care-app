509a86534ec6ae02fed6f0ffe7ef4a2870560514
NunTheeKnee <jane6677doe@gmail.com>
2024-09-02 13:20:32 +0530

    Initialized the auth

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get 'home/old_about', to: redirect('home/about')
+  get 'login', to: 'logins#new'
+  get 'login/create', to: 'logins#create', as: :create_login
   # Defines the root path route ("/")
   # root "posts#index"
 end
