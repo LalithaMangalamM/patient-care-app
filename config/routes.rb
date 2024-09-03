5b73823498ce3f75c8f660b1122fa676e5189220
Abhishek Srivastava <abhishek77in@gmail.com>
2024-09-03 23:14:49 +0530

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   get 'performance_test/complex_haml_component'
   get 'performance_test/complex_erb_component'
 
+  get 'performance_test/complex_haml_view'
+  get 'performance_test/complex_erb_view'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
