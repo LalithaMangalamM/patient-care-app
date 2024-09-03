1fba8ab654ab435f8cf2fbc3a5f1856c210137d5
NunTheeKnee <jane6677doe@gmail.com>
2024-09-03 16:10:24 +0530

    Auth Update: Fixed alerts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,12 +11,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     user = User.from_omniauth(auth)
     if user.present?
-      sign_out_all_scopes
       flash[:success] = t "devise.omniauth_callbacks.success", kind: "Google"
       sign_in_and_redirect user, event: :authentication
     else
-      flash[:alert] = t "devise.omniauth_callbacks.failure", kind: "Google", reason: "#{auth.info.email} is not authorized"
-      redirect_to new_user_session_path
+      flash[:alert] = t "devise.omniauth_callbacks.failure", kind: "Google", reason: "User does not exist. Kindly Sign Up"
+      redirect_to new_user_registration_path(email: auth.info.email)
     end
   end
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,19 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
   def self.from_omniauth(auth)
-    user = where(email: auth.info.email).first_or_initialize do |u|
+    user = where(email: auth.info.email).first do |u|
       u.provider = auth.provider
       u.uid = auth.uid
     end
 
-    user.avatar_url = auth.info.image
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.save
-    user
+    if user.present?
+      user.avatar_url = auth.info.image
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+      user
+    else
+      nil
+    end
   end
 end
