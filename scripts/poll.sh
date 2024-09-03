fe38c54920cc16534c309cca7ae395edabe70683
Abhishek Srivastava <abhishek77in@gmail.com>
2024-09-04 00:05:28 +0530

    Update poll.sh

--- scripts/poll.sh
+++ scripts/poll.sh
@@ -1,6 +1,18 @@
 for i in {1..100}
 do
-  echo "Waiting 1s"
-  # curl https://example.com
-  sleep 1
+  echo "Load complex haml component..."
+  curl https://patient-care-7f88cdf8910d.herokuapp.com/performance_test/complex_haml_component
+  sleep 0.25
+
+  echo "Load complex erb component..."
+  curl https://patient-care-7f88cdf8910d.herokuapp.com/performance_test/complex_erb_component
+  sleep 0.25
+
+  echo "Load complex haml view..."
+  curl https://patient-care-7f88cdf8910d.herokuapp.com/performance_test/complex_haml_view
+  sleep 0.25
+
+  echo "Load complex haml view..."
+  curl https://patient-care-7f88cdf8910d.herokuapp.com/performance_test/complex_erb_view
+  sleep 0.25
 done
